0efb20833039e84db2bab9217caa4917e8ea63d2
Maycem_AMOR <maycem.amor@gmail.com>
2024-04-15 11:21:53 +0200

    - Connecting Currency Conversion Microservice & API Gateway with Zipkin.

--- src/main/java/com/mytech/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/com/mytech/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -2,6 +2,9 @@ package com.mytech.microservices.currencyconversionservice.controller;
 
 import com.mytech.microservices.currencyconversionservice.CurrencyExchangeServiceProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,8 +15,21 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+@Configuration(proxyBeanMethods = false)
+class RestTemplateConfiguration {
+
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
+
+
 @RestController
 public class CurrencyConversionController {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Autowired
     private CurrencyExchangeServiceProxy proxy;
@@ -28,7 +44,7 @@ public class CurrencyConversionController {
         uriVariables.put("from", from);
         uriVariables.put("to", to);
 
-        ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+        ResponseEntity<CurrencyConversionBean> responseEntity = restTemplate.getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
                 CurrencyConversionBean.class,
                 uriVariables);
         CurrencyConversionBean response = responseEntity.getBody();
